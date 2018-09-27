77d53cbb73357f9954397de75df34e5da4f3be98
ming <jianmingforpc@gmail.com>
2018-09-27 16:39:18 +0800

    整理代码

--- main.go
+++ main.go
@@ -1,37 +1,10 @@
 package main
 
 import (
-	"syscall"
-	"fmt"
-	"os"
-	"os/signal"
-	"ngrok-client/ngrokc/config"
-	"ngrok-client/ngrokc/connection"
+	"ngrok-client/ngrokc"
 )
 
 func main() {
-
-	config.ParseConfig()
-
-	var ccon = connection.ControlConnection{}
-
-	// 除了关闭信号
-	signalChan := make(chan os.Signal)
-	signal.Notify(signalChan, os.Interrupt, os.Kill, syscall.SIGHUP, syscall.SIGTERM, syscall.SIGQUIT)
-	go exit(signalChan, &ccon)
-
-	ccon.Init(config.CONFIG.ServerHostname, config.CONFIG.ServerPort, config.CONFIG.User, config.CONFIG.Password)
-	ccon.SetHTTPConfig(config.CONFIG.HttpHostname, config.CONFIG.HttpSubdomain, config.CONFIG.HttpAuth, config.CONFIG.HttpLocalPort)
-	ccon.SetHTTPSConfig(config.CONFIG.HttpsHostname, config.CONFIG.HttpsSubdomain, config.CONFIG.HttpsAuth, config.CONFIG.HttpsLocalPort)
-	ccon.Service()
-
+	ngrokc.Start()
 }
 
-func exit(signalChan chan os.Signal, ccon *connection.ControlConnection) {
-
-	sign := <- signalChan
-
-	fmt.Println(sign)
-
-	ccon.Close()
-}
--- ngrokc/ngrokc.go
+++ ngrokc/ngrokc.go
@@ -1,9 +1,37 @@
 package ngrokc
 
 import (
+	"syscall"
 	"fmt"
+	"os"
+	"os/signal"
+	"ngrok-client/ngrokc/config"
+	"ngrok-client/ngrokc/connection"
 )
 
 func Start() {
-	fmt.Println("1")
+
+	config.ParseConfig()
+
+	var ccon = connection.ControlConnection{}
+
+	// 除了关闭信号
+	signalChan := make(chan os.Signal)
+	signal.Notify(signalChan, os.Interrupt, os.Kill, syscall.SIGHUP, syscall.SIGTERM, syscall.SIGQUIT)
+	go exit(signalChan, &ccon)
+
+	ccon.Init(config.CONFIG.ServerHostname, config.CONFIG.ServerPort, config.CONFIG.User, config.CONFIG.Password)
+	ccon.SetHTTPConfig(config.CONFIG.HttpHostname, config.CONFIG.HttpSubdomain, config.CONFIG.HttpAuth, config.CONFIG.HttpLocalPort)
+	ccon.SetHTTPSConfig(config.CONFIG.HttpsHostname, config.CONFIG.HttpsSubdomain, config.CONFIG.HttpsAuth, config.CONFIG.HttpsLocalPort)
+	ccon.Service()
+
+}
+
+func exit(signalChan chan os.Signal, ccon *connection.ControlConnection) {
+
+	sign := <- signalChan
+
+	fmt.Println(sign)
+
+	ccon.Close()
 }
