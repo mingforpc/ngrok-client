9507fdf670631a010c564ba7dedbb49f0562528d
ming <jianmingforpc@gmail.com>
2018-09-30 17:35:16 +0800

    添加注释

--- ngrokc/ngrokc.go
+++ ngrokc/ngrokc.go
@@ -9,22 +9,29 @@ import (
 	"syscall"
 )
 
+// Ngrok client的启动函数
 func Start() {
 
+	// 异常退出时的处理
 	defer exceptionPrecess()
 
+	// 配置文件的解析
 	config.ParseConfig()
 
 	var ccon = connection.ControlConnection{}
 
-	// 除了关闭信号
+	// 处理关闭信号
 	signalChan := make(chan os.Signal)
 	signal.Notify(signalChan, os.Interrupt, os.Kill, syscall.SIGHUP, syscall.SIGTERM, syscall.SIGQUIT)
 	go exit(signalChan, &ccon)
 
+	// 初始化 control connection
 	ccon.Init(config.CONFIG.ServerHostname, config.CONFIG.ServerPort, config.CONFIG.User, config.CONFIG.Password)
+	// 设置HTTP的配置
 	ccon.SetHTTPConfig(config.CONFIG.HttpHostname, config.CONFIG.HttpSubdomain, config.CONFIG.HttpAuth, config.CONFIG.HttpLocalPort)
+	// 设置HTTPS的配置
 	ccon.SetHTTPSConfig(config.CONFIG.HttpsHostname, config.CONFIG.HttpsSubdomain, config.CONFIG.HttpsAuth, config.CONFIG.HttpsLocalPort)
+	// 开始服务
 	err := ccon.Service()
 
 	fmt.Println(err)
